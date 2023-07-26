a2f44184b4b57ff8e834804ff9fbc7abdc9a5fe8
earthsoo <128710723+earthsoo@users.noreply.github.com>
2023-07-26 14:33:20 +0900

    Update led_ex01.py

--- led_ex01.py
+++ led_ex01.py
@@ -10,7 +10,8 @@ LED = 8
 #set led initial value
 GPIO.setup(LED,GPIO.OUT, initial=GPIO.LOW)
 
-#try
+#try구문인데, while로만 무한루프
+#0.5초 간격으로 high,low벙갈아 가며 바꾸기
 try:
     while 1:
         GPIO.output(LED,GPIO.HIGH)
@@ -19,6 +20,8 @@ try:
         GPIO.output(LED,GPIO.LOW)
         time.sleep(0.5)
 
+#다음과 같은 상황은  터미널에서 모형을 돌리는 중에 Ctrl+c 를 눌렀거나
+#주피터 노트북에서 모형을 놀리고 있는중에 stop을 할 때, session에서 돌아가고 있는 모형이 날라가는 것을 방지해준다
 except KeyboardInterrupt:
     pass
     
