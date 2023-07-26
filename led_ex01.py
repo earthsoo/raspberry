3f5e56a0af71ec14b2979390e466f6889fd42301
earthsoo <128710723+earthsoo@users.noreply.github.com>
2023-07-26 14:28:28 +0900

    Update led_ex01.py

--- led_ex01.py
+++ led_ex01.py
@@ -1,12 +1,16 @@
 import RPi.GPIO as GPIO
+#library to control GPIO
 import time
 
-GPIO.setmode(GPIO.BOARD)
+#Set pinmode
+GPIO.setmode(GPIO.BOARD) 
 
 LED = 8
 
+#set led initial value
 GPIO.setup(LED,GPIO.OUT, initial=GPIO.LOW)
 
+#try
 try:
     while 1:
         GPIO.output(LED,GPIO.HIGH)
