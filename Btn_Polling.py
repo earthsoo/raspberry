2aaa57cd6d9dceb1a7cf63613478e53385c06d25
earthsoo <128710723+earthsoo@users.noreply.github.com>
2023-07-28 12:09:29 +0900

    Update Btn_Polling.py

--- Btn_Polling.py
+++ Btn_Polling.py
@@ -1,10 +1,15 @@
 import RPi.GPIO as GPIO
 import time
 
+#BCM모드로 핀 설정, 버튼 핀은 15번
 BUTTON = 15;
 GPIO.setmode(GPIO.BCM)
 
+#GPIO입력으로 설정, PULLUP/PULLDOWN 설정
+#이 코드는 풀다운 == BUTTON눌렀을 때(반응 있을 시에) 반응하도록 설정
 GPIO.setup(BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+#BUTTON의 인풋이 HIGH(3.3V)일 때 반응
 while 1:
 	if GPIO.input(BUTTON)==GPIO.HIGH:
 		print("Button pushed")
